3a08aad505a9216d7e668b17f8d001137216de93
Braian Dos Santos <102192666+Braianvladzgrid@users.noreply.github.com>
2026-05-07 00:30:48 +0000

    update 2 header braian

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,13 +2,27 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+};
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
-                <Outlet />
+            <Outlet />
             <Footer />
         </ScrollToTop>
     )
