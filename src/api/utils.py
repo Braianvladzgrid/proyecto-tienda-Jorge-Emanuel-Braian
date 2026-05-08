9bd59e43c6d8e97f1f036182bd2bde0b75a3908a
Jorgeotero1998 <jorgeotero1998@gmail.com>
2026-05-08 20:22:43 -0300

    feat: integracion de Cloudinary para imagenes y Mailgun para correos

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,41 +1,23 @@
-from flask import jsonify, url_for
+import cloudinary
+import cloudinary.uploader
+import os
+import requests
 
-class APIException(Exception):
-    status_code = 400
+cloudinary.config( 
+  cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"), 
+  api_key = os.getenv("CLOUDINARY_API_KEY"), 
+  api_secret = os.getenv("CLOUDINARY_API_SECRET") 
+)
 
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+def upload_image(file):
+    upload_result = cloudinary.uploader.upload(file)
+    return upload_result["secure_url"]
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
-
-def generate_sitemap(app):
-    links = ['/admin/']
-    for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
-        if "GET" in rule.methods and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            if "/admin/" not in url:
-                links.append(url)
-
-    links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
-    return """
-        <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
-        <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
-        <p>Start working on your project by following the <a href="https://start.4geeksacademy.com/starters/full-stack" target="_blank">Quick Start</a></p>
-        <p>Remember to specify a real endpoint path like: </p>
-        <ul style="text-align: left;">"""+links_html+"</ul></div>"
+def send_recovery_email(to_email, token):
+    return requests.post(
+        f"https://api.mailgun.net/v3/{os.getenv('MAIL_DOMAIN')}/messages",
+        auth=("api", os.getenv('MAILGUN_API_KEY')),
+        data={"from": "Soporte Tienda <mailgun@YOUR_DOMAIN_HERE>",
+              "to": [to_email],
+              "subject": "Recuperacion de contrasenia",
+              "text": f"Tu token es: {token}"})
